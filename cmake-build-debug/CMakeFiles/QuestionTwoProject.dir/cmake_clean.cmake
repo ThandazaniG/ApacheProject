e4fec23fabf26bfedc93acd92c4637f0a0deb7c7
thandazanig <thandazanigwampa99@gmail.com>
2023-08-21 22:10:15 +0200

    "added project build files"

---
+++ cmake-build-debug/CMakeFiles/QuestionTwoProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QuestionTwoProject.dir/main.c.o"
+  "QuestionTwoProject"
+  "QuestionTwoProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/QuestionTwoProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
